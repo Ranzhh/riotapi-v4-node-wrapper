f05a2c69d951be02760b351b76ea5b0951699793
Matteo Ranzetti <ranzetti.matteo@gmail.com>
2019-10-24 15:37:25 +0200

    Show basic example of index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import { key } from "./config.json";
 import Crescendo from "./crescendo";
 
 const test = async () => {
-
+    const crescendo = new Crescendo(key);
+    const matchData = await crescendo.matchV4.getMatch("matchid", "region").then(console.log).catch(console.log);
 };
 
 test();
