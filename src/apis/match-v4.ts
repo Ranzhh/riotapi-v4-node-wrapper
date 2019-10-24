616311c71bfdf71cff9833850e16edee9c17b6ef
Matteo Ranzetti <ranzetti.matteo@gmail.com>
2019-10-24 15:37:08 +0200

    Add MatchV4:GET_getMatch support

--- src/apis/match-v4.ts
+++ src/apis/match-v4.ts
@@ -1,7 +1,23 @@
+import { reject, resolve } from "bluebird";
+import { netcall } from "../netcall";
+
 export class MatchV4 {
     private key: string;
 
     constructor(key: string) {
         this.key = key;
+    }
+
+    /**
+     * Get match by match ID.
+     * @link https://developer.riotgames.com/api-methods/#match-v4/GET_getMatch
+     * @param matchID - the Match ID of the game.
+     * @param region - the Region on which the match was played.
+     * @returns {Promise<IMatchDto>} Promise of the match data for the game.
+     */
+    public async getMatch(matchID: string, region: string): Promise<IMatchDto> {
+        return await netcall(region, `match/v4/matches/${matchID}`, this.key)
+        .then((res: IMatchDto) => resolve(res))
+        .catch((err: IRiotApiError) => reject(err));
     }
 }
