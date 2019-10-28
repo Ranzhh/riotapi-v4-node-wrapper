741993ab07860c82afdb387faa62de914317fc4f
Matteo Ranzetti <ranzetti.matteo@gmail.com>
2019-10-28 12:28:43 +0100

    Add SummonerV4:GET_getBySummonerName support

--- src/apis/summoner-v4.ts
+++ src/apis/summoner-v4.ts
@@ -9,7 +9,7 @@ export class SummonerV4 {
     }
 
     /**
-     * Get summoner by account ID.
+     * Get a summoner by account ID.
      * @link https://developer.riotgames.com/apis#summoner-v4/GET_getByAccountId
      * @param encryptedAccountId - the encrypted account ID of the summoner.
      * @param region - the Region on which the summoner plays.
@@ -17,6 +17,19 @@ export class SummonerV4 {
      */
     public async getSummonerByAccountId(encryptedAccountId: string, region: string): Promise<ISummonerDto> {
         return await netcall(region, `summoner/v4/summoners/by-account/${encryptedAccountId}`, this.key)
+            .then((res: ISummonerDto) => resolve(res))
+            .catch((err: IRiotApiError) => reject(err));
+    }
+
+    /**
+     * Get a summoner by summoner name.
+     * @link https://developer.riotgames.com/apis#summoner-v4/GET_getBySummonerName
+     * @param summonerName - the name of the summoner.
+     * @param region - the Region on which the summoner plays.
+     * @returns {Promise<ISummonerDto>} Promise of the summoner data.
+     */
+    public async getSummonerByName(summonerName: string, region: string): Promise<ISummonerDto> {
+        return await netcall(region, `summoner/v4/summoners/by-name/${summonerName}`, this.key)
             .then((res: ISummonerDto) => resolve(res))
             .catch((err: IRiotApiError) => reject(err));
     }
