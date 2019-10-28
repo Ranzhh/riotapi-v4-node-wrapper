f4397c06701f751b1f6bf17ef6f3b879d6b22863
Matteo Ranzetti <ranzetti.matteo@gmail.com>
2019-10-28 12:26:31 +0100

    Add SummonerV4:GET_getByAccountId support

--- src/apis/summoner-v4.ts
+++ src/apis/summoner-v4.ts
@@ -7,4 +7,17 @@ export class SummonerV4 {
     constructor(key: string) {
         this.key = key;
     }
+
+    /**
+     * Get summoner by account ID.
+     * @link https://developer.riotgames.com/apis#summoner-v4/GET_getByAccountId
+     * @param encryptedAccountId - the encrypted account ID of the summoner.
+     * @param region - the Region on which the summoner plays.
+     * @returns {Promise<ISummonerDto>} Promise of the summoner data.
+     */
+    public async getSummonerByAccountId(encryptedAccountId: string, region: string): Promise<ISummonerDto> {
+        return await netcall(region, `summoner/v4/summoners/by-account/${encryptedAccountId}`, this.key)
+            .then((res: ISummonerDto) => resolve(res))
+            .catch((err: IRiotApiError) => reject(err));
+    }
 }
